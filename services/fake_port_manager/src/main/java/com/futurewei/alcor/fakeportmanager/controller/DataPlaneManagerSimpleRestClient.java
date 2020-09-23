78e8f54a825e69729df26f4273eb67255d1ac41c
Wei Yuan <dakuaiera@sina.com>
2020-09-23 15:21:28 +0800

    remove useless codes

--- services/fake_port_manager/src/main/java/com/futurewei/alcor/fakeportmanager/controller/DataPlaneManagerSimpleRestClient.java
+++ services/fake_port_manager/src/main/java/com/futurewei/alcor/fakeportmanager/controller/DataPlaneManagerSimpleRestClient.java
@@ -11,7 +11,7 @@ import org.springframework.web.client.RestTemplate;
 
 
 @Configuration
-public class DataPlaneManagerBulkRestClient {
+public class DataPlaneManagerSimpleRestClient {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
